4e391156f7f8ff937169ae6de79dda50811fe9f5
Khurshid <axurshidbek2005@gmail.com>
2022-07-30 02:57:11 +0500

    modded

--- swiper.js
+++ swiper.js
@@ -1,4 +1,17 @@
 var swiper = new Swiper(".mySwiper", {
-    effect: "cards",
-    grabCursor: true,
-  });
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
